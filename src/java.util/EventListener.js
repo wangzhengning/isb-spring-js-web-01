c401b623596daa2406984bed2c81775dae6a5c86
wangzhengning <a1256761520@163.com>
2017-08-07 09:45:26 +0800

    add new files

---
+++ src/java.util/EventListener.js
@@ -0,0 +1,12 @@
+/**
+* 所有事件侦听器接口必须扩展的标记接口
+**/
+var EventListenerManage = {
+    options:{},
+    init:function (){
+
+    }，
+    bindEvent:function(){
+
+    }
+}
---
+++ src/java.util/Formattable.js
@@ -0,0 +1,18 @@
+/**
+* Formattable接口必须通过需要执行自定义格式化（使用Formatter的‘s’转换说明符）的任何类来实现。
+**/
+var FormattableManage = {
+    options:{},
+    init:function (){
+      
+    }，
+    bindEvent:function(){
+
+    }，
+    /**
+    * 使用提供的格式器来格式化对象
+    **/
+    formatTo(Object formatter , Object flages , Object width , Object precision){
+
+    }
+}
