13f0e0cf64047b75eb2de597e46f122c81844ecb
wangzhengning <a1256761520@163.com>
2017-08-07 15:33:42 +0800

    add file SortedMap.js  SortedSet.js

---
+++ src/java.util/SortedMap.js
@@ -0,0 +1,41 @@
+/**
+* SortedMap:
+* 根据键的自然顺序进行排序。
+**/
+var SortedMapManage = {
+    options:{},
+    init:function (){
+
+    }，
+    bindEvent:function(){
+
+    },
+    comparator:function(){
+
+    },
+    entrySet:function(){
+
+    },
+    firstKey:function(){
+
+    },
+    headMap:function(var toKey){
+
+    },
+    keySet:function(){
+
+    },
+    lastKey:function(){
+
+    },
+    subMap:function(var fromKey , var tokey){
+
+    },
+    tailMap:function(var fromKey){
+
+    },
+    values:function(){
+
+    }
+
+}
---
+++ src/java.util/SortedSetManage.js
@@ -0,0 +1,31 @@
+/**
+* SortedSet:
+* 根据键的自然顺序进行排序。
+**/
+var SortedMapManage = {
+    options:{},
+    init:function (){
+
+    }，
+    bindEvent:function(){
+
+    },
+    comparator:function(){
+
+    },
+    first:function(){
+
+    },
+    headSet:function(var toElement){
+
+    },
+    last:function(){
+
+    },
+    subSet:function(var fromElement , var toElement){
+
+    },
+    tailSet:function(var fromElement){
+
+    }
+}
